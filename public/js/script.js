bb0e4ef951f0877af4b38292dbe288dc97eb09cc
786-JIYA <jiyas9610@gmail.com>
2026-04-09 18:41:07 +0530

    Bismillah

--- public/js/script.js
+++ public/js/script.js
@@ -3,20 +3,28 @@ const socket = io();
 // Initialize map with a default view
 const map = L.map("map").setView([0, 0], 15);
 
+
 //Bismillah HirRhman Nir -Raheem
 
+// --- STATIC STOP MARKER ---
+
+
 const stop = {
-  name: "College Gate",
-  lat: 16.7105,
-  lng: 74.2500
+    name: "Market Yard (Stop 3)",
+    lat: 16.8511318,
+    lng:74.5727574
+    
+
+
 };
 
 const stopMarker = L.marker([stop.lat, stop.lng])
-  .addTo(map)
-  .bindPopup("📍 Market Yard");
+    .addTo(map)
+    .bindTooltip(`📍 ${stop.name}`, {
+        permanent: true,
+        direction: "top"
+    });
 
-
-  
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "Map data © OpenStreetMap contributors"
 }).addTo(map);
@@ -28,39 +36,62 @@ socket.on("receive-location", (data) => {
     console.log("Recaived Location", data);
     const { id, lat, lng, time, speed } = data;
 
+    //Bismillah Hir-Rahman NirRheem
+    const distance = map.distance(
+    [lat, lng],
+    [stop.lat, stop.lng]
+); // distance in meters
+
+let eta = "Calculating...";
+
+if (speed > 0) {
+    const speed_mps = speed * (1000 / 3600); // convert km/h → m/s
+
+    const timeSeconds = distance / speed_mps;
+
+    const minutes = Math.floor(timeSeconds / 60);
+    const seconds = Math.floor(timeSeconds % 60);
+
+    eta = `${minutes} min ${seconds} sec`;
+} else {
+    eta = "Bus stopped";
+}
+
     // Update map center
-    map.setView([lat, lng], 16);
+    // map.setView([lat, lng], 16);
+ 
 
     // Update or create marker
     if (busMarker[id]) {
         busMarker[id].setLatLng([lat, lng])
-            .setPopupContent(`🚌 College Bus<br>⏱ ${time}<br>🚀 ${speed} km/h`);
-        
+            .setPopupContent(`🚌 College Bus<br>
+⏱ ${time}<br>
+🚀 ${speed} km/h<br>
+📍 ETA: ${eta}`);
+
     } else {
         busMarker[id] = L.marker([lat, lng])
             .addTo(map)
-            .bindPopup(`🚌 College Bus<br>⏱ ${time}<br>🚀 ${speed} km/h`)
+            .bindPopup(`🚌 College Bus<br>
+⏱ ${time}<br>
+🚀 ${speed} km/h<br>
+📍 ETA: ${eta}`)
             .openPopup();
     }
+
+    const bounds = L.latLngBounds([
+        [stop.lat, stop.lng],
+        [lat, lng]
+    ]);
+
+    map.fitBounds(bounds, { padding: [50, 50] });
 });
 
 
 
 
 
-socket.on("receive-location", (data) => {
-  const { lat, lng } = data;
 
-  // If marker already exists → move it
-  if (busMarker) {
-    busMarker.setLatLng([lat, lng]);
-  } else {
-    // First time → create marker
-    busMarker = L.marker([lat, lng]).addTo(map);
-  }
 
-  // 👉 IMPORTANT: call ETA calculation
-  updateETA(lat, lng);
-});
 
 
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const http = require('http');
